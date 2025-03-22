32a359843ef272b19ec94ee6749bdda5e8506aad
EV <evgenviktorov88@gmail.com>
2025-03-22 20:22:40 +0200

    initial

--- js/task-3.js
+++ js/task-3.js
@@ -1,3 +1,11 @@
 console.log('-- task-3 --');
 
 // ==============
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername('Marco');
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
